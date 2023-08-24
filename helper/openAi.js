fb52feb5c31eb6fa5466aefae5b5b6a2376d64db
Showrov Islam <63032286+showrov29@users.noreply.github.com>
2023-08-24 10:53:27 +0600

    feat: #3 openai integration

--- helper/openAi.js
+++ helper/openAi.js
@@ -2,14 +2,14 @@ const { OpenAI } = require("openai");
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_TOKEN, // defaults to process.env["OPENAI_API_KEY"]
 });
-const demo = async () => {
+const demo = async (userMessage) => {
 	const response = await openai.chat.completions.create({
-		messages: [{ role: "user", content: "Say this is a test" }],
-		model: "ext-davinci-003",
+		messages: [{ role: "user", content: userMessage }],
+		model: "gpt-3.5-turbo",
 		temperature: 1,
 	});
 
-	console.log(response.choices);
+	return response.choices[0].message.content;
 };
 
 module.exports = { demo };
