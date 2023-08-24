e6e4781a0da87c2c8bccc1bcf79e175517e49e60
Showrov Islam <63032286+showrov29@users.noreply.github.com>
2023-08-25 00:34:57 +0600

    refactor: #2 refactor checkNotice

--- helper/openAi.js
+++ helper/openAi.js
@@ -2,7 +2,7 @@ const { OpenAI } = require("openai");
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_TOKEN, // defaults to process.env["OPENAI_API_KEY"]
 });
-const demo = async (userMessage) => {
+const generateOpenAiResponse = async (userMessage) => {
 	const response = await openai.chat.completions.create({
 		messages: [{ role: "user", content: userMessage }],
 		model: "gpt-3.5-turbo",
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 
-const axios = require("axios");
-const cheerio = require("cheerio");
-const OpenAI = require("openai");
 const { handleDoc } = require("./helper/handleDoc");
-const { demo } = require("./helper/openAi");
+
+const { checkForNewNotices } = require("./helper/checkNotice");
+const { downloadAudio } = require("./helper/youtubeDownload");
 
 // Replace 'YOUR_TOKEN' with your bot's API token
 const TOKEN = process.env.BOT_TOKEN;
@@ -14,43 +13,20 @@ const bot = new TelegramBot(TOKEN, {
 	polling: true,
 });
 
-const websiteURL = "https://www.aiub.edu/category/notices";
-
-// Keep track of previously notified notices
-const notifiedNotices = new Set();
-
-// Function to check for new notices
-async function checkForNewNotices() {
-	try {
-		const response = await axios.get(websiteURL);
-		const $ = cheerio.load(response.data);
-
-		// Extract and process notices
-		$("ul.event-list li").each((index, element) => {
-			const day = $(element).find("time span.day").text().trim();
-			const month = $(element).find("time span.month").text().trim();
-			const link = $(element).find("a.info-link").attr("href");
-			const title = $(element).find("div.info h2").text().trim();
-
-			const noticeText = `Time: ${day} ${month} \nTitle: ${title}\nLink: https://www.aiub.edu/${link}`;
-
-			if (!notifiedNotices.has(noticeText)) {
-				// Send notification for new notice
-				bot.sendMessage(1628337716, `New notice: ${noticeText}`);
-				notifiedNotices.add(noticeText);
-			}
-		});
-	} catch (error) {
-		console.error("Error fetching or parsing website:", error);
-	}
-}
-
 // Set interval to periodically check for new notices
 const intervalMinutes = 30; // Set the interval in minutes
-setInterval(checkForNewNotices, intervalMinutes * 60 * 1000);
+setInterval(() => {
+	checkForNewNotices(bot);
+}, intervalMinutes * 60 * 1000);
 
 // Command handler for /start command
-
+bot.onText(/\/start/, (msg) => {
+	const chatId = msg.chat.id;
+	bot.sendMessage(
+		chatId,
+		"Hello! I'm your bot. Send me a message or a document."
+	);
+});
 // Message handler
 bot.on("message", async (msg) => {
 	const chatId = msg.from.id;
@@ -63,7 +39,7 @@ bot.on("message", async (msg) => {
 
 	// Check if the received message is a text message
 	if (msg.text) {
-		bot.sendMessage(chatId, msg.text);
+		downloadAudio(bot, msg);
 	}
 });
 
